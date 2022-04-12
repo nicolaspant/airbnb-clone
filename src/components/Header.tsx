fb37c637e3feb9ee821e6e1889313c0f09d14a26
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-11 20:53:24 -0400

    added static components <header/> <hero/> <search/>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 
 const Header: FC = () => {
-    return <div>Header</div>
+    return <div className = "flex justify-between px-2 py-2 bg-white shadow-md">
+        <img className = "w-12 "src = "/images/airbnblogo.png" alt = "Logo"/>
+        <button className="text-xs border border-2 border-blue-500 rounded-md px-4">Sign In</button>
+    </div>
 }
 
 export default Header;
